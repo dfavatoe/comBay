328e58ac688c85492da3a425ab7d54de68c220c9
dfavatoe <d.favato@outlook.com>
2025-02-24 22:47:22 +0100

    Saved cartModels.js

--- server/models/cartsModel.js
+++ server/models/cartsModel.js
@@ -4,19 +4,19 @@ const { Schema } = mongoose;
 
 const cartsSchema = Schema({
   _id: Schema.Types.ObjectId,
-  cProducts: [
+  products: [
     {
       //Total by Type of Product
-      cProductByType: {
-        pTypeObject: { type: Schema.Types.ObjectId, ref: "Product" },
-        pTypeQty: { type: Number, require: true },
-        pTypeTotal: { type: Number },
-        pTypeDiscountedTotal: { type: Number },
+      productKind: {
+        product: { type: Schema.Types.ObjectId, ref: "Product" },
+        productQty: { type: Number, require: true },
+        productTotal: { type: Number },
+        productDiscountedTotal: { type: Number },
       },
       //Total for all Types of Products
-      pTotalQty: { type: Number },
-      pTotal: { type: Number },
-      ser: { type: Schema.Types.ObjectId, ref: "User" },
+      productsTotalQty: { type: Number },
+      productsTotal: { type: Number },
+      user: { type: Schema.Types.ObjectId, ref: "User" },
       timestamp: {
         timestamps: {
           createdAt: "created_at",
