51f04c61fcaf6a52eb11f7a189a6546ccb73aab3
dfavatoe <d.favato@outlook.com>
2025-03-27 19:45:52 +0100

    Added URL in production mode in urls.ts

--- client/src/utils/urls.ts
+++ client/src/utils/urls.ts
@@ -1,3 +1,7 @@
-const baseUrl = "http://localhost:5100";
+//once the client is deployed, we should modify the fetch urls to the live one. To make it flexible, we could create an .env variable for the deployed URL and a utility variable to select the development or production one
+const baseUrl =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_LOCAL_HOST
+    : import.meta.env.VITE_SERVER_URL;
 
 export { baseUrl };
