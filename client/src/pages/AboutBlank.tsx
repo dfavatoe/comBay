c92c7af8b17b2bd4612b25096a8b9a3c494eb1e0
dfavatoe <d.favato@outlook.com>
2025-03-29 17:32:06 +0100

    Added image URL to the AboutBlank page

--- client/src/pages/AboutBlank.tsx
+++ client/src/pages/AboutBlank.tsx
@@ -9,7 +9,10 @@ function AboutBlank() {
       <h2>Got lost?</h2>
       <p>The requested page doesn't exist.</p>
       <Container className="w-75 d-flex justify-content-center">
-        <Image src="../src/assets/404_picture.jpg" fluid />
+        <Image
+          src="https://res.cloudinary.com/dsf34lqnj/image/upload/v1740753749/comBay/cd4o6m2cyqomqgkhnrhe.jpg"
+          fluid
+        />
       </Container>
       <Button
         className="mt-4 mb-2"
